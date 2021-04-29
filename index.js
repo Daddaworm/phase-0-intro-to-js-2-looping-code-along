1467bbb56d7175b4632924e949e138e4e8dd9810
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-04-29 16:21:26 -0400

    commit tree 53870418974d55be7b27d0500c706596b6f0af95

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 
 
-function writeCards(names, event) {
-    let array = []
-    for (let i = 0; i < names.length; i++) {
-        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
+//function writeCards(names, event) {
+//    let array = []
+//   for (let i = 0; i < names.length; i++) {
+//        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+//    }
+//    return array;
+//}
+
+//function countDown(i){
+//    while(i>=0){
+//    console.log(i--);
+//  }
+//}
+
+
+let array = []
+function writeCards(names, event){
+    for(let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]} for the wonderful ${event} gift!`);
     return array;
-}
-
-function countDown(i)
-{
-    while(i>=0)
-    {
-        console.log(i--);
     }
-
 }
+
+
+function countDown(i){
+    while(i >= 0){
+      console.log(i--)
+    }
+  }
